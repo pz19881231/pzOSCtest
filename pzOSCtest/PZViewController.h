dc4d9b40ba05ffdb473db7f81399ea5050f518b3
Paul <pz19881231@126.com>
2013-06-01 20:37:03 +0200

    test

--- pzOSCtest/PZViewController.h
+++ pzOSCtest/PZViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PZViewController : UIViewController
+#import "AsyncUdpSocket.h"
+
+@interface PZViewController : UIViewController{
+    long tag;
+	AsyncUdpSocket *udpSocket;
+}
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+@property (nonatomic, strong) AsyncUdpSocket *socket;
+@property (weak, nonatomic) IBOutlet UISlider *mySlider;
+
+
+
+
 
 @end
--- pzOSCtest/PZViewController.m
+++ pzOSCtest/PZViewController.m
@@ -7,17 +7,35 @@
 //
 
 #import "PZViewController.h"
+//#define FORMAT(format, ...) [NSString stringWithFormat:(format), ##__VA_ARGS__]
 
 @interface PZViewController ()
+//@property (strong,nonatomic) UInt16 * kUDP_PORT;
+
 
 @end
+
 
 @implementation PZViewController
 
 - (void)viewDidLoad
 {
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    udpSocket = [[AsyncUdpSocket alloc] initWithDelegate:self];
+	
+	//NSError *error = nil;
+	/*
+	if (![udpSocket bindToPort:0 error:&error])
+	{
+		NSLog(@"Error binding: %@", error);
+		return;
+	}
+	*/
+	[udpSocket receiveWithTimeout:-1 tag:0];
+	
+	NSLog(@"Ready");
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +43,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)didSliderChange:(id)sender {
+    int value = (int)self.mySlider.value;
+    NSString *msg =[NSString stringWithFormat:@"p%i",value];
+    self.myLabel.text=msg;
+    NSString *host = @"192.168.1.177" ;
+    int port = 80;
+  
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    //[self logMessage:FORMAT(@"SENT (%i): %@", (int)tag, msg)];
+    tag++;
+    //NSLog(@"hello");
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    NSString *host = @"192.168.1.177" ;
+    int port = 80;
+    NSString *msg =@"hello";
+    NSData *data = [msg dataUsingEncoding:NSUTF8StringEncoding];
+    [udpSocket sendData:data toHost:host port:port withTimeout:-1 tag:tag];
+    //[self logMessage:FORMAT(@"SENT (%i): %@", (int)tag, msg)];
+    tag++;
+    NSLog(@"hello");
+
+}
+
+
+
+
 
 @end
